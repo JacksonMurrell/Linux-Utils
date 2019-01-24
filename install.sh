009574de0df4ed6f21ef6ea0b8af7f91a7d7af5b
Jackson Murrell <acejhm@gmail.com>
2019-01-23 23:05:51 -0600

    Moved vimrc installation into a function.

--- install.sh
+++ install.sh
@@ -18,12 +18,23 @@ else
 fi
 
 # Copy over vimrc configuration.
-cp $vimrc_path "~/.vimrc"
-
+setup_vim "/home/jackson"
 if [ "$EUID" -ne 0 ]
 then
     echo "Cannot configure 'root' vimrc.  Please run as sudo if you wish for this to be configured."
 else
     cp $vimrc_path "/root/.vimrc"
+    setup_vim '/root'
 fi
 
+
+setup_vim()
+{
+    cp $vimrc_path "$1/.vimrc"
+
+    # Copy over initial Vundle vim plugin repository.
+    git clone https://github.com/VundleVim/Vundle.vim.git /$1/.vim/bundle/Vundle.vim
+
+    # Initialize Vundle.
+    vim +BundleInstall +qall
+}
