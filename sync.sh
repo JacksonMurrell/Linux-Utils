47842a30d69ce4a91aad9d5303578a10e0dd045d
Jackson <jackson.m.murrell@protonmail.com>
2019-05-16 14:52:32 -0500

    Hopefully resolved some sync script git issues.
    -- Changed everything to use the non-aliased version for
       behavioral consistency.
    -- Also documented all of the sync script so I know what I'm
       even trying to do.

--- sync.sh
+++ sync.sh
@@ -1,21 +1,38 @@
-git branch temp
-git checkout temp
+# All the git commands have a \ so as to enforce default behavior.
+# This is to get rid of unexpected behavior due to aliasing commands.
+
+# Create a branch to do the merge on.
+\git branch temp
+# Go to this branch.
+\git checkout temp
+# Copy over the local changes to the repo.
 cp ~/.bashrc bash/bashrc
 cp ~/.aliases bash/aliases
 cp ~/.vimrc vim/vimrc
-git merge master
+# Get the latest changes from origin, but don't pull.
+\git fetch origin
+# Merge the latest changes from origin into the branch.
+\git merge --verbose origin/master
 
-conflict=`git diff --check`
+# Determine if there are any unmerged conflicts.
+conflict=`git diff --diff-filter=U`
 
+# If there is no conflict, we can go ahead and merge with problems.
 if [ -z "$conflict" ]
 then
-    git add *
-    git commit -m "Auto-generated commit from sync."
-    git push
-    git checkout master
-    git merge temp
-    git branch -d temp
-    git branch --delete temp
+    # Add and then commit all of our changed files.
+    \git add *
+    \git commit -m "Auto-generated commit from sync."
+    # Push those changes to the temp branch, and make it on the server to preserve the current state.
+    \git push --set-upstream-origin temp
+    # Switch back to master and merge in our new branch.
+    \git checkout master
+    \git pull
+    \git merge --verbose -m "Auto-generated merge from sync." temp
+    # Delete the temp branch since we don't need it anymore.
+    \git branch -d temp
+    \git branch --delete temp
+# Otherwise we will need to merge manually and resolve differences.
 else
     echo "There were unresolvable conflicts in your version and the remote version.  Please resolve them manually."
     exit -1
